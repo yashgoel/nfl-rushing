d1e0577befd77876805d0494eee536511855055c
Yash Goel <yash.goel@thescore.com>
2017-06-08 11:48:35 -0400

    Sorting done now

--- app/controllers/nfl_controller.rb
+++ app/controllers/nfl_controller.rb
@@ -1,5 +1,21 @@
 class NflController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @players = Player.all
+    @players = Player.order("#{sort_column} #{sort_direction}")
   end
+
+  private
+    def sortable_columns
+      ["player", "team", "position", "avg_attempts_per_game", "total_attempts", "total_yards", "average_yards_per_attempt", "yards_per_game", "total_touchdowns", "longest_rush", "first_downs", "first_down_percentage", "twenty_yards_plus_each", "forty_yards_plus_each", "fumbles"]
+    end
+
+    def sort_column
+      sortable_columns.include?(params[:column]) ? params[:column] : "player"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
+
 end
